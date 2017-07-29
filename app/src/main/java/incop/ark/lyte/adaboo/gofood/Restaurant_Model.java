5d80e52fe80e214dbc35fc026dad0597b75f1bcf
Arnold AZ <arnoldazeem2008@gmail.com>
2017-07-29 12:28:06 +0200

    card view implemented

---
+++ app/src/main/java/incop/ark/lyte/adaboo/gofood/Restaurant_Model.java
@@ -0,0 +1,46 @@
+package incop.ark.lyte.adaboo.gofood;
+
+/**
+ * Created by adaboo on 7/29/17.
+ */
+
+public class Restaurant_Model {
+
+        private String name;
+        private int numOfSongs;
+        private int thumbnail;
+
+        public Restaurant_Model() {
+        }
+
+        public Restaurant_Model(String name, int numOfSongs, int thumbnail) {
+            this.name = name;
+            this.numOfSongs = numOfSongs;
+            this.thumbnail = thumbnail;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getNumOfSongs() {
+            return numOfSongs;
+        }
+
+        public void setNumOfSongs(int numOfSongs) {
+            this.numOfSongs = numOfSongs;
+        }
+
+        public int getThumbnail() {
+            return thumbnail;
+        }
+
+        public void setThumbnail(int thumbnail) {
+            this.thumbnail = thumbnail;
+        }
+    }
+
